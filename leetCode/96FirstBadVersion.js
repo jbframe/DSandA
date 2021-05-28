8c0febde966049a459de73a07a1468e8d5a49305
jbframe <framejb@gmail.com>
2021-05-28 16:07:28 -0600

    O(log(n)) Runtime beats 87.19 % of javascript submissions. Memory usage beats 72.57 % of javascript submissions.

---
+++ leetCode/96FirstBadVersion.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/774/
+/*
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/*
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+ var solution = function(isBadVersion) {
+  /*
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+
+   1 2 3 4 5 6 7 8 9 10 x x
+   */
+  return function(n) {
+      let start = 1;
+      let end = n;
+      let mid = Math.floor((start + end) / 2);
+
+      while(start <= end) {
+          if(isBadVersion(mid)) {
+              if(!isBadVersion(mid - 1)) return mid;
+              end = mid - 1;
+          } else {
+              start = mid + 1;
+          };
+          mid = Math.floor((start + end) / 2);
+      }
+  }
+};
