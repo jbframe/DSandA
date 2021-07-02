9640d081b7471f12fecb2d3246f22a7870ded784
jbframe <framejb@gmail.com>
2021-07-01 21:20:31 -0600

    Added check list!

---
+++ udemy-algorithms-and-data-structures-masterclass/checkList.js
@@ -0,0 +1,15 @@
+/* Checklist
+
+https://www.youtube.com/watch?v=r1MXwyiGi_U&ab_channel=TechLead
+https://www.youtube.com/watch?v=zHczhZn-z30&ab_channel=Cl%C3%A9mentMihailescu
+
+1. Depth First Search
+2. Breadth-First Search
+3. Matching Parenthesis
+4.Making use of Hash Tables
+5. Knowing how to manipulate multiple variables/Pointers at once
+6. Reversing a linked list
+7.Sorting fundamentals  (time complexity, functioning, etc.)
+8. Recursion
+9. Custom Data structure (ex. suffix tree )
+10.Binary Search
